c2558f6fc48157264b05e9a9b41b0093d9110ae6
BumboxTheSequel <113855578+BumboxTheSequel@users.noreply.github.com>
2022-10-29 03:41:16 +0300

    Задание выполнено. Вариант 8.

---
+++ DZ4_Makogon_UTS21/DZ4_1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int arrlen = int.Parse(Console.ReadLine());
+            double arrmin = 101;
+            Random rand = new Random();
+            double[] arr = new double[arrlen];
+            int countNeg = 0;
+            double summ = 0;
+            double Backup;
+            for (int i = 0; i < arrlen; i++)
+            {
+                arr[i] = rand.Next(-20, 20) / 10.0; // максимально число 100, минимальное - -100
+            }
+            Console.Write("\n");
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arrlen; i++)
+            {
+                Console.Write("{0:0.0} \t", arr[i]);
+            }
+            for (int i = 0; i < arrlen; i++)
+            {
+                if (arr[i] < arrmin)
+                {
+                    arrmin = arr[i];
+                }
+            }
+            Console.WriteLine("\n номер минимального элемента массива:\n {0:0.0}", arrmin);
+            for (int i = 0; i < arrlen; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    countNeg += 1;
+                }
+                else
+                if (countNeg == 1)
+                {
+                    summ += arr[i];
+                }
+                else if (countNeg > 1)
+                {
+                    break;
+                }
+            }
+            Console.Write("\n Сумма чисел между первым и втрорым отрицательными числами:\n{0:0.0} \n", summ);
+            for (int i = 0; i < arrlen - 1; i++)
+            {
+                
+                if ((Math.Abs(arr[i]) >= 1) && (Math.Abs(arr[i+1]) < 1)) // программа проверяет, есть ли впереди число, удовлетворяющее критерию. При если данное число, на котором остановился цикл, не удовлетворять критериям подбора числа. В противном случае циул просто "перешагнёт" число.
+                {
+                    Backup = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = Backup;
+                    while (i != 0) //число перемещается в самый конец массива, и так происходит с каждым найденным числом. После этого цикл обнуляется и считает заново.
+                    {
+                        i -= 1;
+                        Backup = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = Backup;
+                    }
+                }
+            }
+            Console.WriteLine("Массив после сортировки:");
+            for (int i = 0; i < arrlen; i++)
+            {
+                Console.Write("{0:0.0} \t", arr[i]);
+            }
+        }
+        }
+}
---
+++ DZ4_Makogon_UTS21/DZ4_2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_2
+{
+    internal class Program
+    {
+        //ВАРИАНТ 8!
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк и столбцов:");
+            int k = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] arr = new int[k, m];
+            int Backup;
+            int summ = 0;
+            int summFull = 0;
+            Random rand = new Random();
+            int[] charac = new int[m];
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = rand.Next(-100, 100);
+                }
+            }
+            Console.WriteLine("Двумерная матрица:");
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine("\n");  
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0:00}\t",arr[i, j]);   
+                }
+            }
+            for (int i = 0; i < m;i++) // cоставление матрицы характеристик
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if ((arr[j, i] % 2 == -1) && (arr[j,i] < 0))
+                    {
+                        summ += Math.Abs(arr[j, i]);
+                    }
+                }
+                charac[i] = summ;
+                summ = 0;
+            }
+            for (int i = 0; i < m-1; i++)
+            {
+                if (charac[i] > charac[i+1]) // cортировка матрицы характеристик, а затем и столбцов в двумерной матрице
+                {
+                    Backup = charac[i + 1];
+                    charac[i + 1] = charac[i];
+                    charac[i] = Backup;
+                    for (int j = 0; j < k; j++)
+                    {
+                        Backup = arr[j, i + 1];
+                        arr[j, i+1] = arr[j, i];
+                        arr[j, i] = Backup;
+                    }
+                    i = -1;
+                }
+              
+            }
+            Console.WriteLine("\nДвумерная матрица после сортировки:");
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0:00}\t", arr[i, j]);
+                }
+            }
+            for (int i = 0; i < m; i++) // Расчёт и печать сумм элементов столбцов с отрицательными элементами
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if ((arr[j, i] < 0))
+                    {
+                        for (int n = 0; n < k; n++)
+                        {
+                            summFull += arr[n, i];
+                        }
+                        break;
+                    }
+                }
+                Console.Write("\n Cумма всех элементов для {0:0}-го столбца:\n", i + 1);
+                Console.WriteLine(summFull);        
+                summFull = 0;
+            }
+        }
+    }
+}
